e50202733da19ea141b08202534d8988f1175d1e
Boba-Syu <1914808114@qq.com>
2019-05-29 22:25:36 +0800

    修改了按钮功能的命令错误

--- app/src/main/java/com/hunau/feed_android/MainActivity.java
+++ app/src/main/java/com/hunau/feed_android/MainActivity.java
@@ -3,6 +3,7 @@ package com.hunau.feed_android;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.hunau.feed_android.cmd.Command;
 
@@ -10,68 +11,116 @@ import transsocket.TcpClient;
 
 public class MainActivity extends AppCompatActivity {
     TcpClient tcpClient;
-    private String FAN = Command.OPENFAN;
-    private String FAN02 = Command.OPENFANTWO;
-    private String GUA = Command.OPENGUA;
-    private String CHUANG = Command.OPENCHUANG;
-    private String TOU = Command.OPENTOU;
-    private String SHUI = Command.OPENSHUI;
+
+    private String FAN = Command.CLOSEFAN;
+    private String FAN02 = Command.CLOSEFANTWO;
+    private String GUA = Command.CLOSEGUA;
+    private String CHUANG = Command.CLOSECHUANG;
+    private String TOU = Command.CLOSETOU;
+    private String SHUI = Command.CLOSESHUI;
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        FAN = Command.CLOSEFAN;
+        FAN02 = Command.CLOSEFANTWO;
+        GUA = Command.CLOSEGUA;
+        CHUANG = Command.CLOSECHUANG;
+        TOU = Command.CLOSETOU;
+        SHUI = Command.CLOSESHUI;
     }
 
     public void link(View view) throws Exception {
         tcpClient = new TcpClient();
+        if (tcpClient == null)
+            textView.setText("建立连接失败!");
+        else
+            textView.setText("建立连接成功!");
     }
 
     public void fan(View view) throws Exception {
         if (tcpClient != null) {
+            FAN = FAN.equals(Command.CLOSEFAN) ? Command.OPENFAN : Command.CLOSEFAN;
             tcpClient.sendMsg(FAN);
-            FAN = FAN == Command.CLOSEFAN ? Command.OPENFAN : Command.CLOSEFAN;
-        } else
+            if (FAN.equals(Command.OPENFAN))
+                textView.setText("已打开风扇1!");
+            else
+                textView.setText("已关闭风扇1!");
+        } else {
             System.out.println("请先建立连接!");
+            textView.setText("请先建立连接!");
+        }
     }
 
     public void fanTwo(View view) throws Exception {
         if (tcpClient != null) {
+            FAN02 = FAN02.equals(Command.CLOSEFANTWO) ? Command.OPENFANTWO : Command.CLOSEFANTWO;
             tcpClient.sendMsg(FAN02);
-            FAN02 = FAN == Command.CLOSEFANTWO ? Command.OPENFANTWO : Command.CLOSEFANTWO;
-        } else
+            if (FAN02.equals(Command.OPENFANTWO))
+                textView.setText("已打开风扇2!");
+            else
+                textView.setText("已关闭风扇2!");
+        } else {
             System.out.println("请先建立连接!");
+            textView.setText("请先建立连接!");
+        }
     }
 
     public void gua(View view) throws Exception {
         if (tcpClient != null) {
+            GUA = GUA.equals(Command.CLOSEGUA) ? Command.OPENGUA : Command.CLOSEGUA;
             tcpClient.sendMsg(GUA);
-            GUA = GUA == Command.CLOSEGUA ? Command.OPENGUA : Command.CLOSEGUA;
-        } else
+            if (GUA.equals(Command.OPENGUA))
+                textView.setText("已打开刮粪板!");
+            else
+                textView.setText("已关闭刮粪板!");
+        } else {
             System.out.println("请先建立连接!");
+            textView.setText("请先建立连接!");
+        }
     }
 
     public void chuang(View view) throws Exception {
         if (tcpClient != null) {
+            CHUANG = CHUANG.equals(Command.CLOSECHUANG) ? Command.OPENCHUANG : Command.CLOSECHUANG;
             tcpClient.sendMsg(CHUANG);
-            CHUANG = CHUANG == Command.CLOSECHUANG ? Command.OPENCHUANG : Command.CLOSECHUANG;
-        } else
+            if (CHUANG.equals(Command.OPENCHUANG))
+                textView.setText("已打开窗帘!");
+            else
+                textView.setText("已关闭窗帘!");
+        } else {
             System.out.println("请先建立连接!");
+            textView.setText("请先建立连接!");
+        }
     }
 
     public void tou(View view) throws Exception {
         if (tcpClient != null) {
+            TOU = TOU.equals(Command.CLOSETOU) ? Command.OPENTOU : Command.CLOSETOU;
             tcpClient.sendMsg(TOU);
-            TOU = TOU == Command.CLOSETOU ? Command.OPENTOU : Command.CLOSETOU;
+            if (TOU.equals(Command.OPENTOU))
+                textView.setText("已打开投食!");
+            else
+                textView.setText("已关闭投食!");
         } else
             System.out.println("请先建立连接!");
     }
 
     public void shui(View view) throws Exception {
         if (tcpClient != null) {
+            SHUI = SHUI.equals(Command.CLOSESHUI) ? Command.OPENSHUI : Command.CLOSESHUI;
             tcpClient.sendMsg(SHUI);
-            SHUI = SHUI == Command.CLOSESHUI ? Command.OPENSHUI : Command.CLOSESHUI;
-        } else
+            if (SHUI.equals(Command.OPENSHUI))
+                textView.setText("已打开喂水!");
+            else
+                textView.setText("已关闭喂水!");
+        } else {
             System.out.println("请先建立连接!");
+            textView.setText("请先建立连接!");
+        }
     }
 }
